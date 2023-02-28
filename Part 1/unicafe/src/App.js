0957b56de2a89cbc19ca509a92cd031eff304afd
Destro001 <byaswanth2003@gmail.com>
2023-02-28 16:30:26 +0530

    Part 1 --> unicafe
    Unicafe Step 2 is done

--- Part 1/unicafe/src/App.js
+++ Part 1/unicafe/src/App.js
@@ -11,7 +11,9 @@ const App = ()=>{
   const [good,setGood] = useState(0);
   const [neutral,setNeutral] = useState(0);
   const [bad,setBad] = useState(0);
-
+  const total = good+bad+neutral
+  const average = (good - bad) / total;
+  const positive = good/total * 100;
   const handleGood =()=> setGood(good+1)
   const handleNeutral =()=> setNeutral(neutral+1)
   const handleBad =()=> setBad(bad+1)
@@ -27,6 +29,9 @@ const App = ()=>{
       <p> Good : {good}</p>
       <p> Neutral : {neutral}</p>
       <p> Bad : {bad}</p>
+      <p> All : {total}</p>
+      <p>Average : {average}</p>
+      <p>Positive : {positive} %</p>
     </div>
   )
 }
