2235c7a0dad52b4d136d8a289923335fbc24cc9d
Destro001 <byaswanth2003@gmail.com>
2023-02-28 20:52:03 +0530

    Unicafe Step 6 done, anecdotes step1,2 done

--- Part 1/unicafe/src/App.js
+++ Part 1/unicafe/src/App.js
@@ -7,36 +7,78 @@ const Button = ({handleClick,text})=>{
     </button>
   )
 }
-
+const StatisticLine = (props)=>{
+  return(
+      <tr>
+        <td>{props.text}</td>
+        <td>{props.value}{props.text === "Positive" && "%"}</td>
+      </tr>
+    // <p>{props.text} : {props.value}</p>
+  )
+}
 const Statistics = (props)=>{
   if(props.total === 0 ){
     return(<div>No Feedbacks given</div>)
   }
   return(
     <div>
-    <p className="heading">Statistics</p>
-      <p> Good : {props.good}</p>
-      <p> Neutral : {props.neutral}</p>
-      <p> Bad : {props.bad}</p>
-      <p> All : {props.total}</p>
-      <p>Average : {props.average}</p>
-      <p>Positive : {props.positive} %</p>
+    <h2 className="heading">Statistics</h2>
+    <table>
+      <tbody>
+      <StatisticLine text = "Good"     value = {props.good}/>
+      <StatisticLine text = "Neutral"  value = {props.neutral}/>
+      <StatisticLine text = "Bad"      value = {props.bad}/>
+      <StatisticLine text = "All"      value = {props.total}/>
+      <StatisticLine text = "Average"  value =  {props.average}/>
+      <StatisticLine text = "Positive" value = {props.positive}/>
+      </tbody>
+    </table>
     </div>
   )
 }
 
 const App = ()=>{
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+  const [selected,setSelected] = useState(0)
+
   const [good,setGood] = useState(0);
   const [neutral,setNeutral] = useState(0);
   const [bad,setBad] = useState(0);
+  const[points,setPoints] = useState(Array(anecdotes.length).fill(0))
   const total = good+bad+neutral
   const average = (good - bad) / total;
   const positive = good/total * 100;
   const handleGood =()=> setGood(good+1)
   const handleNeutral =()=> setNeutral(neutral+1)
   const handleBad =()=> setBad(bad+1)
+  
+  const handleAnecdotes =()=>{
+    const index = Math.floor(Math.random()*anecdotes.length);
+    console.log(index)
+    setSelected(index)
+  }
+  const handleVote = ()=>{
+    console.log("index",selected)
+    const updatedPoints  = [...points] 
+    updatedPoints[selected]+=1;
+    setPoints(updatedPoints)
+  }
   return(
-    <div>
+    <div className="container">
+      <div>
+        {anecdotes[selected]} has {points[selected]}
+      </div>
+      <Button handleClick={handleAnecdotes} text = "Next Anecdotes"/>
+      <Button handleClick={handleVote} text = "Vote"/>
       <p className="heading">Give Feedback</p>
       <div>
         <Button handleClick={handleGood} text = "Good"/>
